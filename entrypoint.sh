1e6f85f0bc92a47ab02d7e0d318d26df31a0e390
bland328 <bland328@users.noreply.github.com>
2020-01-07 15:53:47 -0600

    Added comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# Clean up
 [[ -e /var/run/dbus.pid ]] && gosu root rm -f /var/run/dbus.pid
 [[ -e /var/run/avahi-daemon/pid ]] && gosu root rm -f /var/run/avahi-daemon/pid
 [[ -e /var/run/dbus/system_bus_socket ]] && gosu root rm -f /var/run/dbus/system_bus_socket
+# Start services
 gosu root service dbus restart
 gosu root service avahi-daemon restart
+# Start Node.js
 npm start -- --userDir /data
