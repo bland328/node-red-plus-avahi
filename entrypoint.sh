412f0f442b107fc1b4aa7058c023b699115af1b4
bland328 <bland328@users.noreply.github.com>
2020-01-08 09:58:49 -0600

    Removed restarting of services

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-# Clean up
+
+# Remove cruft
 [[ -e /var/run/dbus.pid ]] && gosu root rm -f /var/run/dbus.pid
 [[ -e /var/run/avahi-daemon/pid ]] && gosu root rm -f /var/run/avahi-daemon/pid
 [[ -e /var/run/dbus/system_bus_socket ]] && gosu root rm -f /var/run/dbus/system_bus_socket
-# Start services
-gosu root service dbus restart
-gosu root service avahi-daemon restart
+
+# Restart services (no longer needed?)
+#gosu root service dbus restart
+#gosu root service avahi-daemon restart
+
 # Start Node.js
 npm start -- --userDir /data
