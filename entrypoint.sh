e2d7f229d8d5b66e7d4b404bdcf258caff857850
bland328 <bland328@users.noreply.github.com>
2020-01-08 11:44:31 -0600

    Returned to using gosu

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Remove cruft
-[[ -e /var/run/dbus.pid ]] && su-exec root rm -f /var/run/dbus.pid
-[[ -e /var/run/avahi-daemon/pid ]] && su-exec root rm -f /var/run/avahi-daemon/pid
-[[ -e /var/run/dbus/system_bus_socket ]] && su-exec root rm -f /var/run/dbus/system_bus_socket
+[[ -e /var/run/dbus.pid ]] && gosu root rm -f /var/run/dbus.pid
+[[ -e /var/run/avahi-daemon/pid ]] && gosu root rm -f /var/run/avahi-daemon/pid
+[[ -e /var/run/dbus/system_bus_socket ]] && gosu root rm -f /var/run/dbus/system_bus_socket
 
 # Restart services (no longer needed?)
-#su-exec root service dbus restart
-#su-exec root service avahi-daemon restart
+#gosu root service dbus restart
+#gosu root service avahi-daemon restart
 
 # Start Node.js
 npm start -- --userDir /data
